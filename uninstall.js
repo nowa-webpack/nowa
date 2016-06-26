f342a7c34167e17214be931f643f6d3b7835ba13
gbk <ck0123456@gmail.com>
2016-06-26 22:50:35 +0800

    fix

--- uninstall.js
+++ uninstall.js
@@ -2,7 +2,7 @@
 * @Author: gbk
 * @Date:   2016-06-26 10:38:07
 * @Last Modified by:   gbk
-* @Last Modified time: 2016-06-26 11:19:17
+* @Last Modified time: 2016-06-26 22:50:24
 */
 
 'use strict';
@@ -10,11 +10,6 @@
 var os = require('os');
 var path = require('path');
 var exec = require('child_process').exec;
-
-if (/caoke/.test(process.env.HOME)) {
-  process.exit(0);
-  return;
-}
 
 var configRoot = path.join(os.homedir(), '.nowa');
 var rimrafPath = path.join(configRoot, 'install', '.bin', 'rimraf');
